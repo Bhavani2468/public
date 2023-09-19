fbffe0d1d6a3e69a386c2ee71bf2167c28657531
Bhavani2468 <101941701+Bhavani2468@users.noreply.github.com>
2023-09-19 17:16:36 +0530

    Add files via upload

---
+++ customTags/src/main/java/customTags/classes/TagHandler.java
@@ -0,0 +1,89 @@
+package customTags.classes;
+
+import java.sql.*;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class TagHandler extends TagSupport{
+	
+	//private Connection connection;
+	private String table;
+	private String displayColumn;
+	private String valueColumn;
+	
+	
+	/*public Connection getConn() {
+		return connection;
+	}
+*/
+
+	public String getTable() {
+		return table;
+	}
+
+
+	public String getDisplayColumn() {
+		return displayColumn;
+	}
+
+
+	public String getValueColumn() {
+		return valueColumn;
+	}
+
+
+	/*public void setConn(Connection conn) {
+		this.connection = conn;
+	}
+
+*/
+	public void setTable(String table) {
+		this.table = table;
+	}
+
+
+	public void setDisplayColumn(String displayColumn) {
+		this.displayColumn = displayColumn;
+	}
+
+
+	public void setValueColumn(String valueColumn) {
+		this.valueColumn = valueColumn;
+	}
+
+	@Override
+    public int doStartTag() throws JspException {
+        try {
+            pageContext.getOut().print("Hello, Custom Tag!");
+            JspWriter out=pageContext.getOut();
+            Class.forName("org.postgresql.Driver");
+        	Connection connection=DriverManager.getConnection("jdbc:postgresql://192.168.110.48:5432/plf_training","plf_training_admin","pff123");
+            Statement st = connection.createStatement();
+            String query = "select " + displayColumn + ", " + valueColumn + " from " + table;
+            ResultSet rs = st.executeQuery(query);
+            
+            out.print("<select>");
+
+            while (rs.next()) {
+                String displayValue = rs.getString(displayColumn);
+                String optionValue = rs.getString(valueColumn);
+                out.print("<option value=\"" + optionValue + "\">" + displayValue + "</option>");
+            }
+
+            out.print("</select>");
+
+            rs.close();
+            st.close();
+            connection.close();
+
+        } catch (Exception e) {
+            throw new JspException(e);
+        }
+        return SKIP_BODY;
+    }
+
+
+	
+}
