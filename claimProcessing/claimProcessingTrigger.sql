3b54971519f785a4146aaaecb6f30aaef2090904
Bhavani2468 <101941701+Bhavani2468@users.noreply.github.com>
2023-09-25 17:19:06 +0530

    Create claimProcessingTrigger.sql

---
+++ claimProcessing/claimProcessingTrigger.sql
@@ -0,0 +1,66 @@
+
+
+	
+select * from claims;
+
+update claims set clam_remarks='rejt' where clam_id=102;
+
+
+create table claim_flow
+(
+    cf_id serial primary key,
+    cf_clam_id int,
+    cf_clam_status varchar(10),
+    cf_clam_remarks varchar(1000),
+    cf_clam_doc_path varchar(500),
+    cf_clam_date date,
+    cf_clam_processed_by int
+);
+
+
+
+select * from claim_flow;
+
+CREATE TABLE Bills (
+    clbl_billindex SERIAL PRIMARY KEY,
+        clam_id INT, 
+    clbl_document_title VARCHAR(100),
+    clbl_document_path VARCHAR(255)
+);
+
+
+select * from bills;
+drop table bill;
+SELECT tgname FROM pg_trigger WHERE tgrelid = 'claims'::regclass;
+
+
+
+-- Create the trigger function
+CREATE OR REPLACE FUNCTION insert_claim_flow_on_update()
+RETURNS TRIGGER AS $$
+Declare
+doc_path varchar(500);
+BEGIN
+    -- Check if the UPDATE statement affected the 'clam_status' column
+    IF OLD.clam_remarks <> NEW.clam_remarks THEN
+        -- Insert data into the 'claim_flow' table when the 'Claims' table is updated
+        doc_path:=(select clbl_document_path from bills);
+        INSERT INTO claim_flow (cf_clam_id, cf_clam_status, cf_clam_remarks,cf_clam_doc_path, cf_clam_date, cf_clam_processed_by)
+        VALUES (NEW.clam_id, NEW.clam_status, NEW.clam_remarks, doc_path,CURRENT_DATE, NEW.clam_processed_by);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create the trigger
+CREATE  TRIGGER claim_flow_trigger
+AFTER UPDATE ON Claims
+FOR EACH ROW
+WHEN (OLD.clam_remarks <> NEW.clam_remarks) -- Trigger only on 'clam_status' updates
+EXECUTE FUNCTION insert_claim_flow_on_update();
+
+
+drop trigger insert_claim_flow_trigger
+ drop function insert_claim_flow_on_update()
+
+
