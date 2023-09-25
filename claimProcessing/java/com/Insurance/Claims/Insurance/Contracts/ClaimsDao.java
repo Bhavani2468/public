0d31f76fc33c8e68a4ae80fa583493b8ea8bf3b4
Bhavani2468 <101941701+Bhavani2468@users.noreply.github.com>
2023-09-25 17:30:04 +0530

    Add files via upload

---
+++ claimProcessing/java/com/Insurance/Claims/Insurance/Contracts/ClaimsDao.java
@@ -0,0 +1,24 @@
+package com.Insurance.Claims.Insurance.Contracts;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Insurance.Claims.Insurance.Models.Claim;
+
+
+public interface ClaimsDao {
+
+
+	ArrayList<Claim> getAllClaims();
+
+	ArrayList<Claim> getFilteredClaims(String status);
+
+	Claim getClaimById(int clamId);
+
+	ArrayList<Claim> viewAllClaims();
+
+	Claim viewClaimById(int clamId);
+int editClaimById(int clamId, String clamRemarks, String clamStatus, String clamProcessedAmount);
+
+}
---
+++ claimProcessing/java/com/Insurance/Claims/Insurance/Repository/ClaimService.java
@@ -0,0 +1,70 @@
+package com.Insurance.Claims.Insurance.Repository;
+
+import com.Insurance.Claims.Insurance.Contracts.ClaimsDao;
+import com.Insurance.Claims.Insurance.Contracts.IService;
+import com.Insurance.Claims.Insurance.Models.Claim;
+
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+@Service
+public class ClaimService implements IService {
+	@Autowired
+	ClaimsDao cdao;
+	
+
+	@Override
+	public ArrayList<Claim> getAllClaims() {
+		// TODO Auto-generated method stub
+		System.out.println("hem");
+		return (ArrayList<Claim>)cdao.getAllClaims();
+	}
+
+
+	@Override
+	public ArrayList<Claim> getFilteredClaims(String status) {
+		// TODO Auto-generated method stub
+		return (ArrayList<Claim>)cdao.getFilteredClaims(status);
+	}
+
+
+	@Override
+	public Claim getClaimById(int clamId) {
+		// TODO Auto-generated method stub
+		return cdao.getClaimById(clamId);
+	}
+
+	//Insurance------------
+
+	@Override
+	public ArrayList<Claim> viewAllClaims() {
+		// TODO Auto-generated method stub
+		return (ArrayList<Claim>)cdao.viewAllClaims();
+	}
+
+
+	@Override
+	public Claim viewClaimById(int clamId) {
+		// TODO Auto-generated method stub
+		return cdao.viewClaimById(clamId);
+	}
+
+
+	@Override
+	public int editClaimById(int clamId, String clamRemarks, String clamStatus,String clamProcessedAmount) {
+		// TODO Auto-generated method stub
+		return cdao.editClaimById(clamId,clamRemarks,clamStatus,clamProcessedAmount);
+	}
+
+
+
+	
+
+
+}
